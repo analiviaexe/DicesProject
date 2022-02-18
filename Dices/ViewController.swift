ed630dd59ac605ed2d5ab1c39324c8c45c84cb40
Ana Livia <analivia.exe@gmail.com>
2022-02-18 01:28:34 -0300

    finalizando v1 do projeto dices

--- Dices/ViewController.swift
+++ Dices/ViewController.swift
@@ -11,9 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destino = segue.destination as! SecondViewController
+        if (segue.identifier == "seg1d4") {
+            destino.lados = 4
+            destino.imagem = "1d4"
+        } else if (segue.identifier == "seg1d6"){
+            destino.lados = 6
+            destino.imagem = "1d6"
+        } else if (segue.identifier == "seg1d8"){
+            destino.lados = 8
+            destino.imagem = "1d8"
+        } else if (segue.identifier == "seg1d12"){
+            destino.lados = 12
+            destino.imagem = "1d12"
+        } else if (segue.identifier == "seg1d20"){
+            destino.lados = 20
+            destino.imagem = "1d20"
+        } else {
+            destino.lados = 100
+            destino.imagem = "1d100"
+        }
+        
+    }
+    
 }
 
